6fc3891098dced91b976ad770c1632cd0fad48c4
Soham <1260soham@gmail.com>
2026-03-21 17:02:32 +0530

    Added game config file

---
+++ src/constants.js
@@ -0,0 +1,5 @@
+export const PLAYER_COLORS = ['red', 'blue', 'green', 'yellow'];
+export const CHARACTERS = ['bomber1', 'bomber2', 'bomber3', 'bomber4'];
+
+export const MAX_PLAYERS = 4;
+export const MIN_PLAYERS_TO_START = 2;

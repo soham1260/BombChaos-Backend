10aca84050033979c8b4fb6f1942eb2e56d1ca35
Ganesh Kachare <kaganesh12@gmail.com>
2026-04-09 19:46:35 +0530

    Added user model schema

---
+++ src/models/User.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            minlength: 2,
+            maxlength: 20,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        rating: {
+            type: Number,
+            default: 1000,
+        },
+    },
+    { timestamps: true }
+);
+
+export const User = mongoose.model('User', userSchema);
